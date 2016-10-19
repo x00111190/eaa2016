10853181cfffe6f708a82c68ab9c7ac800874d0e
John Burns <jzburns@gmail.com>
2016-10-19 14:17:28 +0100

    adding newscript.sh file to github

---
+++ week3/newscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+printf "What is your name?"
+
+read name
+
+printf "What month were you born?"
+
+read birthMonth
+
+printf "What is your favourite colour?"
+
+read colour 
+
+printf "Hello $name you were born in $birthMonth and your favourite colour is $colour \n"
